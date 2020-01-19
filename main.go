2b490578a4b95039d3137c8c45e8bb3f4c16e51c
nntakuya <uv.nkmt.yii@gmail.com>
2020-01-19 21:21:13 +0900

    データベース構築

--- main.go
+++ main.go
@@ -2,16 +2,32 @@ package main
 
 import (
   "fmt"
-  "net/http"
+  "database/sql"
+  _ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-  http.HandleFunc("/", handler)
-  http.ListenAndServe(":8080", nil)
-}
+const DRIVER = "mysql"
+const DSN = "root:password@tcp(mysql:3306)/matsun_dev"
 
-func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hello, from Docker container!")
+
+func main() {
+  db, err := sql.Open(DRIVER, DSN)
+  if err != nil {
+      fmt.Println("Openエラー")
+  } else {
+    fmt.Println("Open OK!")
+  }
+
+  err = db.Ping()
+  if err != nil {
+    fmt.Println("接続失敗")
+  } else {
+    fmt.Println("接続OK")
+  }
+
+  db.Close()
+
+
 }
 
 
